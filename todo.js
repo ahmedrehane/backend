fbb84f86c5714ba03950b2bd2b1290e9e34b1066
ahmedrehane <rehaneahmed49@gmail.com>
2024-01-10 11:17:51 +0100

    rest-api with express && mongoose

---
+++ todo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+    id:Number,
+    task:String
+})
+
+const TodoModel = mongoose.model("todos",TodoSchema);
+
+module.exports =  TodoModel
+
+
+
+
